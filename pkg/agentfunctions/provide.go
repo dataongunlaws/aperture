dataongunlaws/aperture

commit e3c28ac888dedb561bf362c2880f7eeff678df3b

Add aperturectl flow-control subcommand and reverse-rpc framework (#1418)
Added a Server-to-client rpc framework (reverse RPC), which allows
controller
to call functions on agents. See
[pkg/rpc/doc.go](https://github.com/fluxninja/aperture/blob/live-preview/pkg/rpc/doc.go)
for details.

Added subcommands:
* `aperturectl agents`
* `aperturectl flow-control control-points`
* `aperturectl flow-control preview [--http] SERVICE CONTROL_POINT`

These subcommands are implemented by `aperturectl` making a request to
controller,
which then makes requests to all agents and aggregates results.

This means that agent connects to controller directly, in addition to
etcd and grpc connections.

`aperturectl` can connect to controller either directly or by
automatically port-forwarding into kubernetes cluster (`--kube`).

docs/gen/config/agent/config-swagger.yaml
@@ -274,6 +274,14 @@ definitions:
         title: BatchPrerollupConfig defines configuration for OTEL batch processor.
         type: object
         x-go-package: github.com/fluxninja/aperture/cmd/aperture-agent/config
+    ClientConfig:
+        properties:
+            grpc:
+                $ref: '#/definitions/GRPCClientConfig'
+                x-go-tag-json: grpc
+        title: ClientConfig is configuration for network clients used by agent-functions.
+        type: object
+        x-go-package: github.com/fluxninja/aperture/pkg/agentfunctions
     ClientTLSConfig:
         properties:
             ca_file:
@@ -320,6 +328,21 @@ definitions:
             https://github.com/kubernetes-sigs/kubebuilder/issues/528
         type: object
         x-go-package: github.com/fluxninja/aperture/cmd/aperture-agent/config
+    Config:
+        properties:
+            client:
+                $ref: '#/definitions/ClientConfig'
+                x-go-tag-json: client
+            endpoints:
+                description: RPC servers to connect to (which will be able to call agent functions)
+                items:
+                    type: string
+                type: array
+                x-go-name: Endpoints
+                x-go-tag-json: endpoints
+        title: Config is configuration for agent functions.
+        type: object
+        x-go-package: github.com/fluxninja/aperture/pkg/agentfunctions
     CustomMetricsConfig:
         description: which will be exported to the controller prometheus.
         properties:

pkg/agentfunctions/provide.go
@@ -1,0 +1,76 @@
+// +kubebuilder:validation:Optional
+package agentfunctions
+
+import (
+	"go.uber.org/fx"
+
+	"github.com/fluxninja/aperture/pkg/config"
+	"github.com/fluxninja/aperture/pkg/info"
+	"github.com/fluxninja/aperture/pkg/log"
+	grpcclient "github.com/fluxninja/aperture/pkg/net/grpc"
+	"github.com/fluxninja/aperture/pkg/rpc"
+)
+
+// ConfigKey is the key for agentfunctions configuration.
+const ConfigKey = "agent_functions"
+
+// Module provides rpc client for agent functions.
+var Module = fx.Options(
+	// FIXME(krdln) Do we actually need a separate grpc client for each module?
+	grpcclient.ClientConstructor{
+		Name:      "agent-functions",
+		ConfigKey: ConfigKey + ".client.grpc",
+	}.Annotate(),
+	fx.Provide(
+		NewControlPointsHandler,
+		ProvidePreviewHandler,
+	),
+	fx.Invoke(
+		RegisterClient,
+		RegisterControlPointsHandler,
+		RegisterPreviewHandler,
+	),
+)
+
+// Config is configuration for agent functions.
+// swagger:model
+// +kubebuilder:object:generate=true
+type Config struct {
+	// RPC servers to connect to (which will be able to call agent functions)
+	Endpoints []string `json:"endpoints"`
+
+	// Network client configuration
+	ClientConfig ClientConfig `json:"client"`
+}
+
+// ClientConfig is configuration for network clients used by agent-functions.
+// swagger:model
+// +kubebuilder:object:generate=true
+type ClientConfig struct {
+	// GRPC client settings.
+	GRPCClient grpcclient.GRPCClientConfig `json:"grpc"`
+}
+
+// RegisterClientIn are parameters for InvokeClient function.
+type RegisterClientIn struct {
+	fx.In
+	Lc           fx.Lifecycle
+	Unmarshaller config.Unmarshaller
+	Handlers     *rpc.HandlerRegistry
+	ConnBuilder  grpcclient.ClientConnectionBuilder `name:"agent-functions"`
+}
+
+// RegisterClient registers a client which will allow calling agent functions from controller.
+func RegisterClient(in RegisterClientIn) error {
+	var config Config
+	if err := in.Unmarshaller.UnmarshalKey(ConfigKey, &config); err != nil {
+		return err
+	}
+
+	for _, addr := range config.Endpoints {
+		rpc.RegisterStreamClient(info.UUID, in.Lc, in.Handlers, in.ConnBuilder.Build(), addr)
+		log.Info().Msgf("Rpc client started, server: %s", addr)
+	}
+
+	return nil
+}

pkg/cmd/provide.go
@@ -1,0 +1,28 @@
+package cmd
+
+import (
+	"go.uber.org/fx"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+
+	cmdv1 "github.com/fluxninja/aperture/api/gen/proto/go/aperture/cmd/v1"
+	"github.com/fluxninja/aperture/pkg/log"
+)
+
+// Module is a module for running cmd.v1.Controller service.
+var Module = fx.Options(
+	fx.Provide(NewHandler),
+	fx.Invoke(RegisterControllerServer),
+)
+
+// RegisterControllerServer registers handler for cmd.v1.Controller service.
+func RegisterControllerServer(handler *Handler, server *grpc.Server, healthsrv *health.Server) {
+	cmdv1.RegisterControllerServer(server, handler)
+
+	healthsrv.SetServingStatus(
+		"aperture.cmd.v1.Controller",
+		grpc_health_v1.HealthCheckResponse_SERVING,
+	)
+	log.Info().Msg("Controller handler registered")
+}

pkg/policies/flowcontrol/service/preview/provide.go
@@ -21,6 +21,9 @@ func Module() fx.Option {
 	)
 }
 
+// ConfigKey is config path where FlowPreviewConfig is located.
+const ConfigKey = "flow_control.preview_service"
+
 // FlowPreviewConfig is the configuration for the flow control preview service.
 // swagger:model
 // +kubebuilder:object:generate=true
@@ -36,7 +39,7 @@ func Register(handler *Handler,
 	unmarshaller cfg.Unmarshaller,
 ) error {
 	var config FlowPreviewConfig
-	if err := unmarshaller.UnmarshalKey("flow_control.preview_service", &config); err != nil {
+	if err := unmarshaller.UnmarshalKey(ConfigKey, &config); err != nil {
 		return err
 	}
 

pkg/rpc/provide.go
@@ -1,0 +1,37 @@
+package rpc
+
+import (
+	"go.uber.org/fx"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+
+	rpcv1 "github.com/fluxninja/aperture/api/gen/proto/go/aperture/rpc/v1"
+	"github.com/fluxninja/aperture/pkg/log"
+)
+
+// ServerModule are components needed for server-side of rpc.
+var ServerModule = fx.Options(
+	fx.Provide(NewClients),
+	fx.Provide(NewStreamServer),
+	fx.Invoke(RegisterStreamServer),
+)
+
+// ClientModule are components needed for client-side of rpc
+//
+// Note: Not providing StreamClient, as this package is generic and doesn't
+// know what to connect to.
+var ClientModule = fx.Provide(NewHandlerRegistry)
+
+// RegisterStreamServer registers the handler on grpc.Server
+//
+// To be used in fx.Invoke.
+func RegisterStreamServer(handler *StreamServer, server *grpc.Server, healthsrv *health.Server) {
+	rpcv1.RegisterCoordinatorServer(server, handler)
+
+	healthsrv.SetServingStatus(
+		"aperture.rpc.v1.Coordinator",
+		grpc_health_v1.HealthCheckResponse_SERVING,
+	)
+	log.Info().Msg("Coordinator handler registered")
+}

api/aperture/cmd/v1/cmd.proto
@@ -1,0 +1,93 @@
+syntax = "proto3";
+
+// Messages for aperturectl → controller and controller → agent communication.
+package aperture.cmd.v1;
+
+import "aperture/flowcontrol/preview/v1/preview.proto";
+import "google/protobuf/empty.proto";
+
+// Controller describes APIs of the controller from the aperturectl POV
+service Controller {
+  rpc ListAgents(google.protobuf.Empty) returns (ListAgentsResponse);
+  rpc ListServices(ListServicesRequest) returns (ListServicesControllerResponse);
+  rpc ListControlPoints(ListControlPointsRequest) returns (ListControlPointsControllerResponse);
+
+  // duplicating a bit preview.v1.FlowPreviewService to keep controller APIs in one place.
+  rpc PreviewFlowLabels(PreviewFlowLabelsRequest) returns (PreviewFlowLabelsControllerResponse);
+  rpc PreviewHTTPRequests(PreviewHTTPRequestsRequest) returns (PreviewHTTPRequestsControllerResponse);
+}
+
+message ListServicesRequest {}
+
+message ListServicesAgentResponse {
+  repeated string services = 1;
+  // FIXME: Remove. Controller should be aware which agent group agent belongs to.
+  string agent_group = 2;
+}
+
+message ListServicesControllerResponse {
+  repeated GlobalService services = 1;
+  uint32 errors_count = 2;
+}
+
+message ListControlPointsRequest {}
+
+message ListControlPointsAgentResponse {
+  repeated ServiceControlPoint control_points = 1;
+  // FIXME: Remove. Controller should be aware which agent group agent belongs to.
+  string agent_group = 2;
+}
+
+message ListControlPointsControllerResponse {
+  repeated GlobalServiceControlPoint control_points = 1;
+  uint32 errors_count = 2;
+}
+
+message ListAgentsResponse {
+  repeated string agents = 1;
+}
+
+// ServiceControlPoint contains info about a control point on a given service.
+//
+// Agent group is implied.
+message ServiceControlPoint {
+  string name = 1;
+  string service_name = 2;
+}
+
+// GlobalServiceControlPoint contains info about a control point on a given
+// service with explicit Agent Group.
+message GlobalServiceControlPoint {
+  string name = 1;
+  string service_name = 2;
+  string agent_group = 3;
+}
+
+// GlobalService is a service with explicit agent group.
+message GlobalService {
+  string name = 1;
+  string agent_group = 2;
+}
+
+// Duplicating preview.PreviewRequest because rpc framework needs separate types.
+
+message PreviewFlowLabelsRequest {
+  string agent_group = 1; // Used only in ctl→controller communication.
+  aperture.flowcontrol.preview.v1.PreviewRequest request = 2;
+}
+
+message PreviewHTTPRequestsRequest {
+  string agent_group = 1; // Used only in ctl→controller communication.
+  aperture.flowcontrol.preview.v1.PreviewRequest request = 2;
+}
+
+// Wrapping response types, as we probably will want to add more
+// controller-specific fields in the future.
+
+message PreviewFlowLabelsControllerResponse {
+  aperture.flowcontrol.preview.v1.PreviewFlowLabelsResponse response = 1;
+}
+
+message PreviewHTTPRequestsControllerResponse {
+  aperture.flowcontrol.preview.v1.PreviewHTTPRequestsResponse response = 1;
+}

api/aperture/plugins/fluxninja/v1/heartbeat.proto
@@ -2,6 +2,7 @@ syntax = "proto3";
 
 package aperture.plugins.fluxninja.v1;
 
+import "aperture/cmd/v1/cmd.proto";
 import "aperture/controlpointcache/v1/kubernetes.proto";
 import "aperture/info/v1/info.proto";
 import "aperture/peers/v1/peers.proto";
@@ -38,7 +39,7 @@ message ReportRequest {
   ServicesList services_list = 8;
   aperture.status.v1.GroupStatus all_statuses = 9;
   aperture.policy.sync.v1.PolicyWrappers policies = 10;
-  repeated ServiceControlPoint service_control_points = 11;
+  repeated cmd.v1.ServiceControlPoint service_control_points = 11;
   repeated aperture.controlpointcache.v1.KubernetesControlPoint kubernetes_control_points = 12;
   string installation_mode = 13;
 }
@@ -69,10 +70,4 @@ message OverlappingService {
   string service1 = 1;
   string service2 = 2;
   int32 entities_count = 3;
-}
-
-// ServiceControlPoint contains info about a control point on a given service.
-message ServiceControlPoint {
-  string name = 1;
-  string service_name = 2;
 }

api/aperture/rpc/v1/rpc.proto
@@ -1,0 +1,62 @@
+syntax = "proto3";
+
+// Messages for aperturectl → controller and controller → agent communication.
+package aperture.rpc.v1;
+
+import "google/protobuf/any.proto";
+import "google/protobuf/duration.proto";
+import "google/rpc/status.proto";
+
+message Request {
+  // Id of the request, this should be unique per client connection. First
+  // request id on the connection should be equal to nextID from the Hello
+  // message.
+  uint64 id = 1;
+
+  // Mandatory timeout for processing request. Timeout is handled on
+  // caller-side. After it passes, caller is no longer interested in a response,
+  // and callee can forget about the request.
+  google.protobuf.Duration timeout = 2;
+
+  // Request payload. The method on the callee will be dispatched by request's type.
+  google.protobuf.Any payload = 3;
+}
+
+message Response {
+  uint64 id = 1;
+  oneof result {
+    // Payload is a message of a type implied by request kind.
+    bytes payload = 2;
+    google.rpc.Status error = 3;
+  }
+}
+
+message ServerToClient {
+  oneof command {
+    // Make a request to client. Expect AgentToController message in the
+    // future containing Response with the same id.
+    Request request = 1;
+
+    // Cancels in-flight request.
+    //
+    // After cancellation, the response is not expected to be received, but
+    // might happen that it will be received. If so, it should be ignored.
+    uint64 cancel_id = 2;
+  }
+}
+
+message ClientToServer {
+  message Hello {
+    string name = 1;
+    uint64 next_id = 2;
+  }
+
+  oneof msg {
+    Hello hello = 1;
+    Response response = 2;
+  }
+}
+
+service Coordinator {
+  rpc Connect(stream ClientToServer) returns (stream ServerToClient);
+}

commit 4bff9e958c1c18a6b1f2e76c9bb45264552b45fd

Fix config with missing agent_functions.endpoints not applying (#1463)
Should fix the following error (introduced by #1418), which occurs when
`agent_functions` is not set in `agents`.

    Agent.fluxninja.com "agent" is invalid:
    spec.config.agent_functions.endpoints: Invalid value: "null":
    spec.config.agent_functions.endpoints in body must be of type array:
    "null". Retrying attempt #5 at 1:57PM.

docs/gen/config/agent/config-swagger.yaml
@@ -339,7 +339,8 @@ definitions:
                     type: string
                 type: array
                 x-go-name: Endpoints
-                x-go-tag-json: endpoints
+                x-go-tag-json: endpoints,omitempty
+                x-go-tag-validate: omitempty,dive,omitempty
         title: Config is configuration for agent functions.
         type: object
         x-go-package: github.com/fluxninja/aperture/pkg/agentfunctions

pkg/agentfunctions/provide.go
@@ -37,7 +37,7 @@ var Module = fx.Options(
 // +kubebuilder:object:generate=true
 type Config struct {
 	// RPC servers to connect to (which will be able to call agent functions)
-	Endpoints []string `json:"endpoints"`
+	Endpoints []string `json:"endpoints,omitempty" validate:"omitempty,dive,omitempty"`
 
 	// Network client configuration
 	ClientConfig ClientConfig `json:"client"`